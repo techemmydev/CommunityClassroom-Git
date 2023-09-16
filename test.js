90ae2220445fff6a4fd3fa11ff6b50410294a0f8
emmytechdev <gabrielemmanuelmary12345@gmail.com>
2023-09-16 08:22:58 +0100

    test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+let user =prompt("if its rainy");
+
+let rainy="rainy"
+if(user=rainy){
+    console.log("take your umbrella");
+
+}
+else {
+    console.log("wear your glass");
+}
+console.log(rainy);
